e1f10a097d1d6d123b0a72b2d3ce39bb40a62cd3
ZukaGap <zurabigaprindashvili@gmail.com>
2020-05-14 22:36:51 +0400

    added copy weather

--- src/Component/days.js
+++ src/Component/days.js
@@ -2,8 +2,31 @@ import React from 'react';
 import "./days.css";
 
 function Day(props){
+    const copy = () => {
+        let day = new Date(props.info.dt * 1000).getDate();
+        let month = new Date(props.info.dt * 1000).getMonth() + 1;
+        let year = new Date(props.info.dt * 1000).getFullYear();
+        let maxT = parseInt(props.info.temp.max,10) + "°(MAX)";
+        let minT = parseInt(props.info.temp.min,10) + "°(MIN)";
+        let wdesc = props.info.weather[0].description;
+        let feels = parseInt(props.info.feels_like.day,10) + "°";
+        let wspeed = props.info.wind_speed;
+        let pres = props.info.pressure + "hPa";
+
+        var dummy = document.createElement('input'),
+        text = day + "/" + month + "/" + year + "\n" + "Temperature: " +
+                 maxT + "/" + minT + "\n" + wdesc + "\n" + "Feeling: " + feels +
+                 "\n" + "Speed: " + wspeed + "\n" + "Pressure: " + pres;
+    
+        document.body.appendChild(dummy);
+        dummy.value = text;
+        dummy.select();
+        document.execCommand('copy');
+        document.body.removeChild(dummy);
+    }
+    
     return (
-    <div className = "ticket">
+    <div className = "ticket" onClick={copy}>
         {console.log(props)}
         <p>
             {new Date(props.info.dt * 1000).getDate()}/
