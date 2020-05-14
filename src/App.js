06e6fdab1c97f7606d6f31dd1616632cd830a0f0
ZukaGap <zurabigaprindashvili@gmail.com>
2020-05-14 16:33:16 +0400

    weather api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Day from "./Component/days"
 
-function App() {
+const App = () => {  
+  const [tbilisi, setTbilisi] = useState([]);  
+  const [kutaisi, setKutaisi] = useState([]);
+  const [batumi, setBatumi] = useState([]);
+
+  const getTodos = async () => {
+    const url_tb = "https://api.openweathermap.org/data/2.5/onecall?lat=41.6941&lon=44.8337&units=metric&exclude=current&appid=f527564fb31cc061c5de78a2e03c9b07";
+    const url_kt = "https://api.openweathermap.org/data/2.5/onecall?lat=42.2496&lon=42.6997&units=metric&exclude=current&appid=f527564fb31cc061c5de78a2e03c9b07";
+    const url_bt = "https://api.openweathermap.org/data/2.5/onecall?lat=41.6416&lon=41.6359&units=metric&exclude=current&appid=f527564fb31cc061c5de78a2e03c9b07";
+
+    const response = await fetch(url_tb);
+    const data = await response.json();
+    setTbilisi(data.daily);
+    const response1 = await fetch(url_kt);
+    const data1 = await response1.json();
+    setKutaisi(data1.daily);
+    const response2 = await fetch(url_bt);
+    const data2 = await response2.json();
+    setBatumi(data2.daily);
+  }
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>      
+      <h2>Tbilisi</h2> 
+      <div className="tbi">
+        {tbilisi.map(todo => (
+          <Day info={todo} key={todo.dt} />
+        ))}
+      </div>
+      <h2>Kutaisi</h2> 
+      <div className="kut">
+        {kutaisi.map(todo => (
+          <Day info={todo} key={todo.dt} />
+        ))}
+      </div> 
+      <h2>Batumi</h2> 
+      <div className="bat">
+        {batumi.map(todo => (
+          <Day info={todo} key={todo.dt} />
+        ))}
+      </div> 
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Component/days.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./days.css";
+
+function Day(props){
+    return (
+    <div className = "ticket">
+        {console.log(props)}
+        <p>
+            {new Date(props.info.dt * 1000).getDate()}/
+            {new Date(props.info.dt * 1000).getMonth() + 1}/
+            {new Date(props.info.dt * 1000).getFullYear()}
+        </p>
+        <div className = "temp">
+            <p className="max">{parseInt(props.info.temp.max,10)}° 
+                <span className = "min">{parseInt(props.info.temp.min,10)}°</span>
+            </p>
+            <p className = "weather_info">{props.info.weather[0].description}</p>
+        </div>
+        <p>Feels: {props.info.feels_like.day}°</p>
+        <p>Wind speed: {props.info.wind_speed}</p>
+        <p>Pressure: {props.info.pressure}kPa</p>
+    </div>
+
+)}
+
+export default Day;
